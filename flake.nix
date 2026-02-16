cb6db3cf6749a930304566b9638f512a66335758
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-02-16 11:39:54 +0900

    debug: show all self attributes

--- flake.nix
+++ flake.nix
@@ -16,10 +16,18 @@
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        # DEBUG: Print self attributes for investigation
-        version = builtins.trace "DEBUG self.ref = ${if builtins.hasAttr "ref" self then self.ref else "UNDEFINED"}"
-                  (builtins.trace "DEBUG self.rev = ${if builtins.hasAttr "rev" self then builtins.substring 0 10 self.rev else "UNDEFINED"}"
-                  (builtins.replaceStrings ["\n"] [""] (builtins.readFile ./VERSION)));
+        # DEBUG: Print ALL self attributes for investigation
+        debugInfo = builtins.trace "=== SELF ATTRIBUTES DEBUG ===" (
+          builtins.trace "self.ref: ${if builtins.hasAttr "ref" self then self.ref else "UNDEFINED"}" (
+          builtins.trace "self.rev: ${if builtins.hasAttr "rev" self then builtins.substring 0 12 self.rev else "UNDEFINED"}" (
+          builtins.trace "self.shortRev: ${if builtins.hasAttr "shortRev" self then self.shortRev else "UNDEFINED"}" (
+          builtins.trace "self.lastModified: ${if builtins.hasAttr "lastModified" self then toString self.lastModified else "UNDEFINED"}" (
+          builtins.trace "self.narHash: ${if builtins.hasAttr "narHash" self then self.narHash else "UNDEFINED"}" (
+          builtins.trace "Available attrs: ${builtins.toString (builtins.attrNames self)}" (
+          builtins.trace "===========================" null
+        )))))));
+
+        version = builtins.seq debugInfo (builtins.replaceStrings ["\n"] [""] (builtins.readFile ./VERSION));
         commit =
           if (builtins.hasAttr "rev" self)
           then (builtins.substring 0 7 self.rev)
